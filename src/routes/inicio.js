abbf768f56d4c7ac8e15d214af790540a911b2d7
BrayanJesusGarcia123 <57869288+BrayanJesusGarcia123@users.noreply.github.com>
2022-11-08 04:44:04 +0100

    brayan

--- src/routes/inicio.js
+++ src/routes/inicio.js
@@ -1,27 +1,14 @@
 //Llamando a Express JS
-const express = require("express");
-const {body} = require("express-validator");
-const router = express.Router();
-const passport = require("passport");
-const { isLoggedIn, isNotLoggedIn } = require("../lib/authentication"); 
+const express = require('express');
 
+//Llamando al modulo de Routers de Express
+
+const router= express.Router();
 
 //Ruta de la pagina inicial(Login del usuario)
 router.get('/', (req,res) => {
     res.render('auth/login')
    
 });
-
-router.post('/signin', (req, res,next )=>{
-    passport.authenticate('local.signin', {
-    successRedirect: '/dashboard',
-    failureRedirect: '/',
-    failureFlash: true
-    })(req,res,next)
- });
-
-
-
-
 
 module.exports= router;
